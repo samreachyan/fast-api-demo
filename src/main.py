eceb4cdeb6b1197ac651cb27092efbc730fabb35
samreachyan <samreachyan@gmail.com>
2023-01-16 02:02:45 +0700

    Update

--- src/main.py
+++ src/main.py
@@ -1,33 +1,36 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Union
 
+from fastapi import FastAPI, Path, Query, Body
+from pydantic import BaseModel, Field
+
+app = FastAPI()
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+    description: Union[str, None] = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: Union[float, None] = None
 
-
-app = FastAPI()
 
 @app.get("/")
 async def get_default():
     return {"message": "Hello all bro!!", "framework": "fast-api"}
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
-
 
 @app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item.dict()}
+async def update_item(item_id: int, item: Item = Body(embed=True)):
+    results = {"item_id": item_id, "item": item}
+    return results
 
 
-@app.get("/items/")
-async def read_items(q: str | None = None):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(title="The ID of the item to get"),
+    q: Union[str, None] = Query(default=None, alias="item-query"),
+):
+    results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
